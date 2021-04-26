f658aea2a42d68f7965c062aa6663a365b5c533d
Konrad9808 <swift.dev.98@gmail.com>
2021-04-26 12:48:56 +0200

    Initial commit

---
+++ Slots App/Slots_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slots_AppApp.swift
+//  Slots App
+//
+//  Created by Szikszai Konrád on 2021. 04. 12..
+//
+
+import SwiftUI
+
+@main
+struct Slots_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
